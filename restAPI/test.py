60404429e648d925538faf93a2e241342e7aebc3
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-02-07 00:14:47 -0800

    Create test.py

---
+++ restAPI/test.py
@@ -0,0 +1,27 @@
+import requests
+
+URL = "http://127.0.0.1:5000/"
+
+
+
+result = requests.post(URL + "/video/" + "1", {"name": "jag", "views":10, "likes":100})
+print("url is ", result.request.url)
+print("body is ", result.request.body)
+print("header is ", result.request.headers)
+print("POST result is ", result.json())
+
+result = requests.get(URL + "/video/" + "1")
+print("url is ", result.request.url)
+print("body is ", result.request.body)
+print("header is ", result.request.headers)
+print("GET result is ", result.json())
+
+# python .\test.py
+# url is  http://127.0.0.1:5000//video/1
+# body is  name=jag&views=10&likes=100
+# header is  {'User-Agent': 'python-requests/2.28.2', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Content-Length': '27', 'Content-Type': 'application/x-www-form-urlencoded'}
+# POST result is  {'data': {'name': 'jag', 'likes': 100, 'views': 10}}
+# url is  http://127.0.0.1:5000//video/1
+# body is  None
+# header is  {'User-Agent': 'python-requests/2.28.2', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
+# GET result is  {'name': 'jag', 'likes': 100, 'views': 10}
