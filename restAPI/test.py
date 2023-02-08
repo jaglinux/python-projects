65b2761054d81bee4a70ae101131d312ca3d877e
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-02-07 23:32:51 -0800

    Update test.py

--- restAPI/test.py
+++ restAPI/test.py
@@ -4,17 +4,47 @@ URL = "http://127.0.0.1:5000/"
 
 
 
-result = requests.post(URL + "/video/" + "1", {"name": "jag", "views":10, "likes":100})
+result = requests.post(URL + "/video/" + "?id=1", {"name": "jag", "views":10, "likes":100})
 print("url is ", result.request.url)
 print("body is ", result.request.body)
 print("header is ", result.request.headers)
+print("Status code is ", result.status_code)
 print("POST result is ", result.json())
 
-result = requests.get(URL + "/video/" + "1")
+input()
+
+result = requests.get(URL + "/video/" + "?id=1")
 print("url is ", result.request.url)
 print("body is ", result.request.body)
 print("header is ", result.request.headers)
+print("Status code is ", result.status_code)
 print("GET result is ", result.json())
+
+print("-------------FAIL----------------------------")
+input()
+
+result = requests.get(URL + "/video/" + "?id=1000")
+print("url is ", result.request.url)
+print("body is ", result.request.body)
+print("header is ", result.request.headers)
+print("Status code is ", result.status_code)
+print("GET result is ", result.json())
+
+input()
+
+result = requests.post(URL + "/video/" + "?id=1", {"name": "jag", "views":10, "likes":100})
+print("url is ", result.request.url)
+print("body is ", result.request.body)
+print("header is ", result.request.headers)
+print("Status code is ", result.status_code)
+print("POST result is ", result.json())
+
+
+
+
+
+
+
 
 # python .\test.py
 # url is  http://127.0.0.1:5000//video/1
